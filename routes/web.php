0a9ab6c51a97ac03f76080aae8a6fa5893035a06
jesus feria <97232222+jfv2007@users.noreply.github.com>
2026-07-05 17:56:36 -0600

    nuevos cambios 2026

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-/* Route::get('/', function () {
+ Route::get('/', function () {
     return view('welcome');
-}); */
+});
 
-Route::get('/admin', function () {
+/* Route::redirect('/', '/dashboard'); */
+
+/*   Route::get('/admin', function () {
     return view('admin.dashboard');
 })->name('admin.dashboard');
+ */
 
-/* Route::redirect('/', '/admin'); */
 
-Route::middleware([
+ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/admin', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
 });
+
+
